d443e74f910ed685c65b266bae2847b91160c2c3
unfoundbug <github@theadmin.co.uk>
2016-01-13 12:51:44 +0000

    Renamed Libraries to Includes these are cross platform includes to ensure CRC / packet definitions are consistant
    Add initial timeout functionality to DalekBob reciever

---
+++ AddJunctions.bat
@@ -0,0 +1,1 @@
+copy "Includes\*.*" DalekBob
--- Includes/Packets.h
+++ Includes/Packets.h
@@ -4,9 +4,18 @@ struct SDalekMotorPacket
 {
   byte byPacketSize = 7;
   byte byPacketVersion = 1;
-  byte byPacketCheckSum;
   byte byPacketID;
   byte byDeviceID;
   byte byPacketDataX, byPacketDataY, byPacketDataZ;
+  int i16PacketRC;
 };
  
+ void AddCRC(SDalekMotorPacket* pPacket)
+ {
+ 
+ }
+ 
+ bool VerifyCRC(SDalekMotorPacket* pPacket)
+ {
+ 
+ }
